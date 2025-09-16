f32f26f961914aedccc93f4456eff3435f7b8305
LinaAragon <linamarce94@gmail.com>
2025-09-16 10:12:45 -0400

    Playing with operators

--- scripts/objects_and_classes.R
+++ scripts/objects_and_classes.R
@@ -69,12 +69,51 @@ colors[!colors == "red"]
 length_m <- c(6, 4.1, 2.8, 5.5, 3.9, 5.8)
 shark_species <- c("Great White Shark", "Lemon Shark", "Bull Shark", 
                    "Hammerhead Shark", "Mako Shark", "Great White Shark")
+
+
+
 length(environment())
 length(length_m)
 length(shark_species)
 
 shark_species |> 
-  unique()
+  unique() |> 
+  length()
 
 mean(length_m)
 max(length_m)
+
+
+# Part B: Vector Operations and Indexing
+# 
+# Extract the first 3 shark species using indexing with [] and save them to an object called first_3
+# Extract shark species where maximum length is greater than 4 meters
+# Assuming the values in length_m and sharks_species are ordered so that they match each other, find the shark species that is the largest
+# Calculate the mean length for all great whites sharks
+
+table1 <- tibble(shark_species,length_m)
+
+
+first_3_o1 <- shark_species[1:3]; first_3_o1
+
+first_3_o2 <- table1[1:3,]; first_3_o2
+
+greater_4_o1 <- shark_species[length_m > 4]; greater_4_o1
+
+greater_4_o2 <- table1$shark_species[table1$length_m > 4]; greater_4_o2
+
+
+# max_shark_o1 <- shark_species[max(length_m)]; max_shark_o1 #This doesn't work beacause it is a coincidence
+
+max_shark_o3 <- shark_species[length_m == max(length_m)]; max_shark_o3
+
+min_shark_o3 <- shark_species[length_m == min(length_m)]; min_shark_o3
+
+max_shark_o2 <- table1$shark_species[length_m == max(table1$length_m)]; max_shark_o2
+
+mean_great <- length_m[shark_species == "Great White Shark"] |> 
+  mean(); mean_great
+
+
+
+
